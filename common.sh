f58365a6a218dbd2ee0b82ea538fb558bffb96c7
ranjithdronadula <dronadularanjith@gmail.com>
2025-06-24 22:10:42 +0530

    catalogue creation

--- common.sh
+++ common.sh
@@ -1,38 +1,89 @@
 #!/bin/bash
 
-START_TIME=$(date +%s)
-USERID="$(id -u)"   # CHECK USER ID 
-R="\e[31m"          
-G="\e[32m"          #BY ADDING COLORS FOR ERRORS SUCCSES
-Y="\e[33m"
-N="\e[0m"
+    START_TIME=$(date +%s)
+    USERID="$(id -u)"   # CHECK USER ID 
+    R="\e[31m"          
+    G="\e[32m"          #BY ADDING COLORS FOR ERRORS SUCCSES
+    Y="\e[33m"
+    N="\e[0m"
 
-LOG_FOLDER="/var/log/roboshop.log"
-SCRIPT_NAME="$(echo $0 | cut -d "." f1)"
-LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
+    LOG_FOLDER="/var/log/roboshop.log"
+    SCRIPT_NAME="$(echo $0 | cut -d "." f1)"
+    LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
 
-mkdir -p $LOG_FOLDER 
-echo -e "$Y This  script started excuting at :: $(date) $N" | tee -a $LOG_FILE  # THIS COMMAND REFER DISPLY THE CONTEND AND STORE IN LOG_FILE
+    mkdir -p $LOG_FOLDER 
+    echo -e "$Y This  script started excuting at :: $(date) $N" | tee -a $LOG_FILE  # THIS COMMAND REFER DISPLY THE CONTEND AND STORE IN LOG_FILE
 
-check_root(){
-    if [ $USERID -ne 0 ]    # CHECK USER ID IF USER ID 0 ITS ROOT USER OTHER WISE NOT ROOT
+    
+
+    app_setup(){
+    id roboshop &>>$LOG_FILE
+    if [ $? -ne 0 ]
     then
-         echo -e "$R ERROR  ::  plese run this script root acces $N" | tee -a $LOG_FILE
-         exit 1  # 1 REFER EXIT THE SCRIPT OR STOP
+        useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
+        VALIDATE $? "Creating roboshop system user"
     else
-         echo -e "$G this script running with root access nothing to do $N" | tee -a $LOG_FILE
-    fi 
-}
- VALIDATE(){
-    if [ $1 -eq 0 ]
-    then 
-        echo -e "$2 ... IS $G SUCCSESS $N"
-    else
-        echo -e "$2.....is $R FAILURE $N "
-         exit 1
-           
-    fi
- }
+        echo -e "system user roboshp already created...$Y SKIPPING $N" | tee -a $LOG_FILE  
+    fi  
+
+    mkdir -p /app 
+    VALIDATE $? "Creating app directory"
+
+    curl -o /tmp/app_name.zip https://roboshop-artifacts.s3.amazonaws.com/app_name-v3.zip &>>$LOG_FILE
+    VALIDATE $? "Downloading app_name"
+
+    rm -rf /app/* # BY REMOVING 2ND TIME WE RUN CATALOUGE 1ST CONTENT REMOVE AND 2ND CONTENT DOWNLAOD
+    cd /app 
+    unzip /tmp/app_name.zip &>>$LOG_FILE
+    VALIDATE $? "unzipping app_name"
+    }
+
+    nodejs_setup(){ 
+    dnf module disable nodejs -y &>>$LOG_FILE
+    VALIDATE $? "Disabling default nodejs"
+
+    dnf module enable nodejs:20 -y &>>$LOG_FILE
+    VALIDATE $? "Enabling nodejs:20"
+
+    dnf install nodejs -y &>>$LOG_FILE
+    VALIDATE $? "Installing nodejs:20"
+
+    npm install &>>$LOG_FILE
+    VALIDATE $? "Installing Dependencies"
+    }
+
+    systemd_setup(){
+    cp $SCRIPT_DIR/app_name.service /etc/systemd/system/app_name.service
+    VALIDATE $? "Copying app_name service"
+
+    systemctl daemon-reload &>>$LOG_FILE
+    systemctl enable app_name  &>>$LOG_FILE
+    systemctl start app_name
+    VALIDATE $? "Starting app_name"
+
+    }
+
+   check_root(){
+        if [ $USERID -ne 0 ]    # CHECK USER ID IF USER ID 0 ITS ROOT USER OTHER WISE NOT ROOT
+        then
+            echo -e "$R ERROR  ::  plese run this script root acces $N" | tee -a $LOG_FILE
+            exit 1  # 1 REFER EXIT THE SCRIPT OR STOP
+        else
+            echo -e "$G this script running with root access nothing to do $N" | tee -a $LOG_FILE
+        fi 
+    }
+    VALIDATE(){
+        if [ $1 -eq 0 ]
+        then 
+            echo -e "$2 ... IS $G SUCCSESS $N"
+        else
+            echo -e "$2.....is $R FAILURE $N "
+            exit 1
+            
+        fi
+    }
+
+
 
   print_time(){
     END_TIME=$(date +%s)
